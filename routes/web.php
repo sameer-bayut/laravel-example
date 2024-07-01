31d48690de33c07d4f27838959e7e8f21746a5df
Muhammad Sameer <muhammad.sameer@dubizzlelabs.com>
2024-07-01 16:17:49 +0500

    added route resource

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','home');
 
-Route::get('/jobs', [JobController::class,'index']);
-Route::post('/jobs',[JobController::class, 'store']);
-Route::get('/jobs/{job}',[JobController::class, 'show']);
-Route::patch('/jobs/{job}',[JobController::class, 'update']);
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
+Route::resource('jobs', JobController::class);
